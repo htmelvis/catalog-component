77d5fbed2eb53a5d1cdfdd05744bf3f82b4846d4
htmelvis <ed@htmelvis.com>
2015-10-19 14:17:54 -0400

    Live Price Added to Product Slideout

--- components/productsslideout.js
+++ components/productsslideout.js
@@ -4,18 +4,26 @@ var React = require('react');
 var classNames = require('classnames');
 
 var ProductSlideOut = React.createClass({
+  getInitialState: function(){
+    return {
+      livePrice: 0
+    }
+  },
   generateProductOptions: function(){
       var options = this.props.selected.productOptions.map(function(option, index){
         return <option value={option.value} key={index}>{option.text}</option>;
       });
      return options;
   },
-  setClasses: function(){
-
-  },
   updateProductOptions: function(){},
   componentDidMount: function(){},
   componentDidUpdate: function(){
+  },
+  updatePrice: function(e){
+    var selectedOption = e.target.options[e.target.options.selectedIndex];
+    var valuePriceUnfiltered = selectedOption.value.split(':')[1];
+    var price = valuePriceUnfiltered.split('|')[0];
+    this.setState({ livePrice: price });
   },
   render: function() {
     var productSlideClasses = classNames({
@@ -38,14 +46,14 @@ var ProductSlideOut = React.createClass({
                 <input type="hidden" name="name" value={this.props.selected.name} />
                 <input type="hidden" name="code" value={this.props.selected.code} />
                 <input type="hidden" name="price" value={this.props.selected.price} />
-                <select name="Product Size">
+                <select onChange={this.updatePrice} name="Product Size">
                   <option value="">Select a size</option>
                   {this.props.selected.productOptions ? this.generateProductOptions() : null}
                 </select>
                 <label>Quantity</label>
                 <input name="quantity" type="text" />
                 <input type="submit" className="button" value="Buy Now" /> <span
-                  className="livePrice">$ {this.props.selected.price}</span>
+                  className="livePrice">$ {this.state.livePrice}</span>
               </form>
               }
 
